99f896386281b4cef119827d578e19edc405de35
kan862 <kanchannaregal@gmailcom>
2025-04-09 14:03:35 +0530

    save java file

---
+++ Add.java
@@ -0,0 +1,10 @@
+class Add{
+    public static void main(String[] args){
+        {
+            int a=10;
+            int b=60;
+            int sum=a+b;
+            System.out.println("sum =" +sum);
+        }
+    }
+}
